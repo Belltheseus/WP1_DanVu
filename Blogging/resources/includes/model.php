e2a82d0d6186b45fb158be0b3b9161c70d380fd1
Belltheseus <42642871+Belltheseus@users.noreply.github.com>
2018-12-10 11:25:58 +0100

    blogging

---
+++ Blogging/resources/includes/model.php
@@ -0,0 +1,35 @@
+<?php
+
+//Simple array for topics - Övning 4
+//$model = array("Första inlägget", "Snart är det vår", "Robin presenterar sig", "Senaste inlägget");
+
+//2D Associative array for full posts - Övning 9
+
+$model = array(
+    "forsta-inlagget" => array(
+        "title" => "första inlägget",
+        "author" => "Gugge",
+        "date" => "2015-01-01",
+        "text" => "Här kommer själva inlägget i sin helhet. Välkommen till Labb 3! Här övar vi på PHP för att bli duktiga webbserverprogrammerare."
+),
+"snart-ar-det-var" => array(
+    "title" => "snart är det vår",
+    "author" => "Gugge",
+    "date" => "2015-02-24",
+    "text" => "Då övar vi på PHP för att bli duktiga webbserverprogrammerare."
+),
+"robin-presenterar-sig" => array(
+    "title" => "Robin presenterar sig",
+    "author" => "Robin",
+    "date" => "2015-02-25",
+    "text" => "Robin är en trevlig prick som gärna övar på PHP för att som du bli en duktig webbserverprogrammerare."
+),
+"senaste-inlagget" => array(
+    "title" => "senaste inlägget",
+    "author" => "Gugge",
+    "date" => "2015-01-01",
+    "text" => " Här övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat."
+)
+);
+
+?>
